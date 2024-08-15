d38cb715c4e127719157ef7fae43acbca737daf2
Uriel Alvarez Valdez <uriel.alvarez.v14@gmail.com>
2024-08-15 15:10:12 -0600

    404 completed

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,5 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 function Page404() {
-  return <div>Pagina no encontrada</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="container-404">
+      <div className="container content-404">
+        <h1>La pagina que buscas no existe</h1>
+        <button
+          className="btn-primary"
+          type="button"
+          onClick={() => navigate("/")}
+        >
+          Ir al inicio
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Page404;
